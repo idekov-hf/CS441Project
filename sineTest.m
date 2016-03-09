87b1991540782da54f6263d940f17eb425f20eb5
Iavor V. Dekov <ivdekov@imac-022291.colby.edu>
2016-03-09 14:27:10 -0500

    added new files

---
+++ sineTest.m
@@ -0,0 +1,4 @@
+t = 0:0.1:24;
+y = sin(t);
+randVal = rand(1);
+plot(t, y + randVal), grid on;
---
+++ timeBasedSineWave.m
@@ -0,0 +1,3 @@
+% http://www.mathworks.com/help/simulink/slref/sinewave.html
+% The output of the Sine Wave block is determined by:
+% y=amplitude×sin(frequency×time+phase)+bias.
